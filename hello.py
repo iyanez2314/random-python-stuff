c92063f1f68a005ef7381736c89a56ef52780207
Isaac Yanez <isaac231467@gmail.com>
2024-01-01 13:54:51 -0600

    random python stuff

---
+++ hello.py
@@ -0,0 +1,20 @@
+# name = input('Please enter your name: ')
+# Remove the white space trailing and leading
+# name = name.strip().title()
+
+# Capitalize the first letter of the name
+# name = name.capitalize()
+
+# Capitalize the first letter of the full name
+# name = name.title()
+
+# Split the name into first and last name
+# first, last = name.split(" ")
+
+
+# print(f'Hello, {first} {last}!')
+
+# print("hello")
+
+
+
